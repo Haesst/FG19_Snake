5c0d883d26ee56b1b488cb4aaa0e4b168b6850ff
Haesst <robin.ortryd@futuregames.nu>
2019-08-26 20:46:26 +0200

    Snake now grows on appleeating Bug: new snake parts gets squeezed in and pushes other parts to the side

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 [RequireComponent(typeof(PlayerInput))]
@@ -7,6 +8,7 @@ public class Snake : MonoBehaviour
     GameController gameController;
     PlayerInput playerInput;
     [SerializeField] GameObject snakeBody;
+    LinkedList<GameObject> snakeBodyParts = new LinkedList<GameObject>();
 
     private int growAmount;
     public int GrowAmount { get => growAmount; set => growAmount = value; }
@@ -36,6 +38,7 @@ public class Snake : MonoBehaviour
             else
             {
                 UpdateHeadPosition();
+                UpdateBodyPosition();
             }
             headPosition = transform.position;
             moveTime = timer;
@@ -52,6 +55,7 @@ public class Snake : MonoBehaviour
             Destroy(collision.gameObject);
             gameController.EatApple();
             growAmount++;
+            timer -= 0.01f;
         }
 
     }
@@ -59,20 +63,34 @@ public class Snake : MonoBehaviour
     {
         Vector3 newPos;
         if (playerInput.Direction == PlayerInput.NORTH)
-            newPos = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+            newPos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
         else if (playerInput.Direction == PlayerInput.EAST)
-            newPos = new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z);
+            newPos = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
         else if (playerInput.Direction == PlayerInput.SOUTH)
-            newPos = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+            newPos = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
         else
-            newPos = new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z);
+            newPos = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
 
         transform.position = newPos;
     }
 
+    private void UpdateBodyPosition()
+    {
+        if(snakeBodyParts.Count > 0)
+        {
+            GameObject lastPart = snakeBodyParts.Last.Value;
+            snakeBodyParts.RemoveLast();
+
+            lastPart.transform.position = headPosition;
+            snakeBodyParts.AddFirst(lastPart);
+        }
+    }
+
     private void AddSnakeBody()
     {
-        Instantiate(snakeBody, headPosition, Quaternion.identity);
+        // Todo: Figure out why it pushes the other parts
+        GameObject gameObject = Instantiate(snakeBody, headPosition, Quaternion.identity);
+        snakeBodyParts.AddFirst(gameObject);
         growAmount--;
     }
 }
